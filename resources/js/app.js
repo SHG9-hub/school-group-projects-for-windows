7a2c20a835d954f240df17c502352887c6d6b472
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-07-29 11:28:52 +0900

    feat: Swiperを追加し、スライダー機能を実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,24 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Swiper
+import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper/bundle';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.mySwiper', {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
